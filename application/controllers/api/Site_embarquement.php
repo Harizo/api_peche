1f5e3686fa89d00d283bcea59196382a9f815b95
Harizo <rajaonarisoazo@gmail.com>
2019-02-07 14:40:45 +0300

    site_embarquement_controller

--- application/controllers/api/Site_embarquement.php
+++ application/controllers/api/Site_embarquement.php
@@ -27,6 +27,7 @@ class Site_embarquement extends REST_Controller {
                 $data['nom'] = $site_embarquement->nom;
                 $data['district_id'] =$site_embarquement->district_id;
                 $data['district_nom'] = $district->nom;
+                $data['district'] = $district;
             }  else {
 				$taiza="findAll no nataony";
                 $menu = $this->Site_embarquementManager->findAll();
@@ -44,8 +45,10 @@ class Site_embarquement extends REST_Controller {
                         $data[$key]['altitude'] = $value->altitude;
                         $data[$key]['district_id'] = $value->id_district;
                         $data[$key]['district_nom'] = $district->nom;
+                        $data[$key]['district'] = $district;
                         $data[$key]['region_id'] = $value->id_region;
                         $data[$key]['region_nom'] = $region->nom;
+                        $data[$key]['region'] = $region;
 
                     }
                 } else
